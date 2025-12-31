e31f577ec8ecaca33718435e8009d2d227ab2577
Nima <nimaj1382@gmail.com>
2025-12-31 19:12:00 +0330

    fix: rename import for association

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,6 @@ from .director import Director
 from .genre import Genre
 from .movie import Movie
 from .rating import Rating
-from .associations import movie_genre_association
+from .associations import MovieGenreAssociation
 
-__all__ = ["Director", "Genre", "Movie", "Rating", "movie_genre_association"]
+__all__ = ["Director", "Genre", "Movie", "Rating", "MovieGenreAssociation"]
