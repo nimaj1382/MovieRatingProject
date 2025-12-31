ea0f0dc729047b2aaae22f8141e4788cae953dd1
Nima <nimaj1382@gmail.com>
2026-01-01 00:56:13 +0330

    feat: add description field for create_director method

--- app/services/director_service.py
+++ app/services/director_service.py
@@ -17,12 +17,17 @@ class DirectorService:
     def get_director_by_name(self, name: str) -> Optional[Director]:
         return self.director_repository.get_by_name(name)
 
-    def create_director(self, name: str, birth_year: int = None) -> Director:
+    def create_director(self, name: str,
+                        birth_year: int = None,
+                        description: str = None,
+                        ) -> Director:
         # Check if director already exists
         existing_director = self.get_director_by_name(name)
         if existing_director:
             raise ExistanceError(f"Director with name '{name}' already exists.")
-        new_director = Director(name=name, birth_year=birth_year)
+        new_director = Director(name=name,
+                                birth_year=birth_year,
+                                description=description)
         self.director_repository.add(new_director)
         return new_director
 
