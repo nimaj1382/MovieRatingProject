12fdf61fb608a128052b84ecbf319df1407674f4
Nima <nimaj1382@gmail.com>
2026-01-01 04:58:40 +0330

    feat: add UniquenessError class

--- app/exceptions/service_exception.py
+++ app/exceptions/service_exception.py
@@ -1,3 +1,6 @@
 class ExistanceError(Exception):
     """Exception raised when a requested resource does not exist."""
     pass
+class UniquenessError(Exception):
+    """Exception raised when a uniqueness constraint is violated."""
+    pass
